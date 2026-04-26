9cf5ff37fb86465d137411e489d5d8b0ccd10d18
Indomitable <yukikazechyan@outlook.com>
2026-04-26 19:30:28 +0800

    fix: 删除空的 CommonEvents 类修复启动崩溃
    问题：
    - CommonEvents 类被 @EventBusSubscriber 注解但没有任何 @SubscribeEvent 方法
    - 导致 mod 加载时抛出 IllegalArgumentException 崩溃
    
    解决：
    - 删除未使用的空 CommonEvents 类

--- src/main/java/com/piranport/CommonEvents.java
+++
@@ -1,7 +0,0 @@
-package com.piranport;
-
-import net.neoforged.fml.common.EventBusSubscriber;
-
-@EventBusSubscriber(modid = PiranPort.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
-public class CommonEvents {
-}
